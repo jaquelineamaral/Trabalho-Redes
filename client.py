f2b028289713d53e9711075c7ca54085756d6d34
vinicius.corbelli <vinicius.corbelli@bling.com.br>
2023-07-12 20:49:31 -0300

    client

--- client.py
+++ client.py
@@ -8,23 +8,13 @@ server_port = 8081
 # Criação do socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Dados sintéticos para envio (10MB)
+data_size = 10 * 1024 * 1024  # 10MB
+data = "0" * data_size
+
 # Configurações do buffer de pacotes
 buffer_size = 10  # Tamanho do buffer em pacotes
 packet_size = 1024  # Tamanho máximo de cada pacote em bytes
-
-# Dados de exemplo a serem enviados
-messages = [
-    "Mensagem 1",
-    "Mensagem 2",
-    "Mensagem 3",
-    "Mensagem 4",
-    "Mensagem 5",
-    "Mensagem 6",
-    "Mensagem 7",
-    "Mensagem 8",
-    "Mensagem 9",
-    "Mensagem 10",
-]
 
 # Variáveis de controle de timeout e retransmissão
 timeout = 2.0  # Tempo em segundos para timeout
@@ -34,11 +24,20 @@ packets_sent = []  # Lista para armazenar os pacotes enviados
 # Função para enviar pacotes
 def send_packet(packet, sequence_number):
     client_socket.sendto(packet.encode(), (server_ip, server_port))
-    print("Enviado pacote com sequência", sequence_number)
+    #print("Enviado pacote com sequência", sequence_number)
+
+# Função para retransmitir pacotes
+def resend_packets(start_sequence_number):
+    for i in range(start_sequence_number, len(data), packet_size):
+        message = data[i:i+packet_size]
+        sequence_number = i // packet_size  # Números de sequência incrementados
+        packet = f"{sequence_number}|{message}"
+        send_packet(packet, sequence_number)
 
 # Enviar pacotes
-for i, message in enumerate(messages):
-    sequence_number = i % (buffer_size * 2)  # Números de sequência entre 0 e 19
+for i in range(0, len(data), packet_size):
+    message = data[i:i+packet_size]
+    sequence_number = i // packet_size  # Números de sequência incrementados
     packet = f"{sequence_number}|{message}"
     send_packet(packet, sequence_number)
     packets_sent.append((packet, sequence_number))
@@ -46,9 +45,10 @@ for i, message in enumerate(messages):
 # Definir o timeout inicial
 start_time = time.time()
 timeout_expired = False
+start_sequence_number = 0
 
 # Aguardar ACKs e tratar timeouts e retransmissões
-while last_ack_received < len(messages) - 1:
+while packets_sent:
     try:
         client_socket.settimeout(timeout)
 
@@ -58,29 +58,26 @@ while last_ack_received < len(messages) - 1:
         print("Recebido ACK:", ack_number)
 
         # Verificar se o ACK é válido
-        if ack_number > last_ack_received:
-            last_ack_received = ack_number
+        if ack_number >= start_sequence_number:
+            start_sequence_number = ack_number + 1
             timeout_expired = False
+
+            # Remover pacotes confirmados da lista de pacotes enviados
+            packets_sent = [(packet, sequence_number) for packet, sequence_number in packets_sent if sequence_number >= start_sequence_number]
 
     except socket.timeout:
         if not timeout_expired:
-            print("Timeout: retransmitindo pacotes a partir da sequência", last_ack_received + 1)
+            print("Timeout: retransmitindo pacotes de sequência", start_sequence_number)
             timeout_expired = True
 
-            i = last_ack_received + 1
-            sequence_number = i % (buffer_size * 2)
-            packet = f"{sequence_number}|{messages[i]}"
-            send_packet(packet, sequence_number)
+            resend_packets(start_sequence_number)
 
     # Verificar se ocorreu timeout
     if time.time() - start_time > timeout:
-        print("Timeout: retransmitindo pacotes a partir da sequência", last_ack_received + 1)
+        print("Timeout: retransmitindo pacotes de sequência", start_sequence_number)
         start_time = time.time()
 
-        i = last_ack_received + 1
-        sequence_number = i % (buffer_size * 2)
-        packet = f"{sequence_number}|{messages[i]}"
-        send_packet(packet, sequence_number)
+        resend_packets(start_sequence_number)
 
 # Fechamento do socket
 client_socket.close()
